bda4524be0435d81e9ae37636b4c708587ca384c
Turnyur <chukunah@gmail.com>
2020-09-22 13:05:55 +0100

    init

---
+++ src/main/java/com/turnyur/springconsumerest/SpringConsumeRestApplication.java
@@ -0,0 +1,13 @@
+package com.turnyur.springconsumerest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringConsumeRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringConsumeRestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/turnyur/springconsumerest/SpringConsumeRestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.turnyur.springconsumerest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringConsumeRestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
